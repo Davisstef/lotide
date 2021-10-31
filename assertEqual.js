823e3853ee306eef622a05a9fafc209563902c66
vagrant <stefan.davis@mail.utoronto.ca>
2021-10-31 19:38:49 +0000

    Regular Assert

--- assertEqual.js
+++ assertEqual.js
@@ -1,24 +1,10 @@
-const tail = function(array) {
-  const newArray = array.slice(1);
-  return newArray;
-};
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Passed: ${actual} === ${expected}`);
+    console.log(actual === expected);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(actual !== expected);
   }
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-
-const words = ["If you don't know, now you know.", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
